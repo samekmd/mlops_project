31f171ca4d74a7a4c7be804e8fe24fb403e06861
samek_md <samuelalkmc@gmail.com>
2026-01-19 09:59:08 -0300

    MLflow part 2

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,7 @@
 import logging
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Configure the logging strategy
 logging.basicConfig(
@@ -9,3 +12,5 @@ logging.basicConfig(
         logging.StreamHandler()
     ]
 )
+
+

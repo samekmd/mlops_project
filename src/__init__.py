233efd00a64a590ecbf3c67f30144181d7456db6
samek_md <samuelalkmc@gmail.com>
2026-01-19 11:25:45 -0300

    MLflow part 4

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,14 @@
 import logging
 from dotenv import load_dotenv
+import dagshub
 
 load_dotenv()
+
+# Initialize DagsHub with credentials
+dagshub.init(
+    repo_owner="samekmd",
+    repo_name="mlops_project"
+)
 
 # Configure the logging strategy
 logging.basicConfig(
